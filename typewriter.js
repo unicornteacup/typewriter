bd1a0c0da07821f7f4135a9dc75b07f458864e79
Caroline <carolinejepps@gmail.com>
2020-06-30 22:03:06 +0000

    added typewriter

---
+++ typewriter.js
@@ -0,0 +1,13 @@
+const sentence = "hello there from lighthouse labs";
+
+
+for (let i = 0; i< sentence.length; i++) {
+  
+  setTimeout(() => {
+    process.stdout.write(sentence[i]);
+    if (sentence.length-1 == i){
+      process.stdout.write('\n');
+    }
+  }, i * 100)
+  
+}
